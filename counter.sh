b303b4a9a1dcf34b4d740c0108fd31e5593c1762
Broken Pipe <54848194+btwiuse@users.noreply.github.com>
2025-06-21 04:12:02 +0800

    update counter

---
+++ counter.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+[[ -d counter ]] || {
+  cargo generate --git https://github.com/CosmWasm/cw-template.git --name counter --define minimal=false
+  pushd counter
+  cargo wasm
+  cargo schema
+  docker run --rm -v "$(pwd)":/code \
+    --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
+    --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+    cosmwasm/optimizer:0.16.1
+  popd
+}
