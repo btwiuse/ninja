f081f64c4f15a6603e0e27f96285fe87993699e6
Broken Pipe <54848194+btwiuse@users.noreply.github.com>
2025-06-25 15:36:26 +0800

    counter_build: add instructions for macOS container cli

--- counter_build.sh
+++ counter_build.sh
@@ -11,5 +11,7 @@ set -e
     --mount type=volume,source="$(basename "$(pwd)")_cache",target=/target \
     --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
     cosmwasm/optimizer:0.16.1
+  # for macOS with apple silicon
+  # container run --rm -v "$(pwd)":/code cosmwasm/optimizer-arm64:0.16.1
   popd
 }
